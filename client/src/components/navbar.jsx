9be7b5435954769fb192cfb94e706eda96f19f03
SunCh1p <blaha.s.connor@gmail.com>
2025-03-01 12:57:00 -0500

    implemented hamburger menu without drop down animation

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from 'react-router-dom';
 import Button from './button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faProjectDiagram, faFileAlt, faUser, faEnvelope, faBlog } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHome, faProjectDiagram, faFileAlt, faUser, faEnvelope, faBlog } from '@fortawesome/free-solid-svg-icons';
 function NavBar(){
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
+
   function handleClick(page){
+    setOpen(false);
     navigate(page)
   }
-
+  function toggleMenu(){
+    setOpen(!open);
+  }
   return(
     <div className="navBarContainer">
-      <span className="titleHighlight">Connor Blaha</span>
-      <div className="hContainer">
+      <h1 className="titleHighlight">Connor Blaha</h1>
+      <div className="hamburger" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <div className={`navButtons ${open ? 'open' : ''}`}>
         <Button onClick={() => handleClick('/')}> <FontAwesomeIcon icon={faHome}/> Home</Button>
         <Button onClick={() => handleClick('/Projects')}> <FontAwesomeIcon icon={faProjectDiagram}/> Projects</Button>
         <Button onClick={() => handleClick('/Resume')}> <FontAwesomeIcon icon={faFileAlt}/> Resume</Button>
-        <Button onClick={() => handleClick('/About')}> <FontAwesomeIcon icon={faUser}/> About me</Button>
+        <Button onClick={() => handleClick('/About')}> <FontAwesomeIcon icon={faUser}/> About</Button>
         <Button onClick={() => handleClick('/Contact')}> <FontAwesomeIcon icon={faEnvelope}/> Contact</Button>
-        <Button onClick={() => handleClick('/Blog')}> <FontAwesomeIcon icon={faBlog}/> Blog</Button>
       </div>
     </div>
   );
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -8,17 +8,23 @@ function Home(){
   return(
     <div>
       <NavBar/>
-      <div className="container">
-        <h1>Hello, I'm Connor Blaha</h1>
-        <p>Future software developer</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <div className="hContainer">
-          <BoxButton>Resume</BoxButton>
-          <BoxButton>Projects</BoxButton>
+      <div className="columnTwo container">
+        <div className = "vContainer">
+          <p>Welcome to my portfolio</p>
+          <h1 className="title">I'm Connor Blaha</h1>
+          <p className="subtitle">Future software developer &lt;/&gt;</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <div className="hContainer">
+            <BoxButton>Resume</BoxButton>
+            <BoxButton>Projects</BoxButton>
+          </div>
+        </div>
+        <div>
+
         </div>
       </div>
     </div>
